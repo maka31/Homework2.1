c227e2db27f2d71a141a06c4fbd3ba896c7ec547
maka31 <makhburat.asanova@gmail.com>
2021-03-25 17:15:39 +0600

    доп задание

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println(goForWalk(31, -15));
         System.out.println(goForWalk(25, 5));
         System.out.println(goForWalk(50, 0));
         System.out.println(goForWalk(86, 6));
         System.out.println(goForWalk(12, -26));
+        System.out.println("------------------------------");
+
+        System.out.println(goForWalk(generateRandomAge(), -15));
+        System.out.println(goForWalk(generateRandomAge(), 5));
+        System.out.println(goForWalk(generateRandomAge(), 6));
+        System.out.println(goForWalk(generateRandomAge(), -26));
     }
 
     public static String goForWalk(int age, int temperature) {
@@ -18,5 +27,13 @@ public class Main {
         } else {
             return "Оставайтесь дома";
         }
+    }
+
+    public static int generateRandomAge(){
+        Random random = new Random();
+       int randomAge = random.nextInt(60);
+        System.out.println("Age: " + randomAge);
+        return randomAge;
+
     }
 }
